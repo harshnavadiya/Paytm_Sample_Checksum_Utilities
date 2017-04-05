aa1b0ffb5b95248563a6aeeae8966ffbac87bfd7
aabhishekgoyal <abhishek.goyal@paytm.com>
2017-04-05 11:36:57 +0530

    GenerateChecksum Updates

--- Ruby/generate_checksum.rb
+++ Ruby/generate_checksum.rb
@@ -13,16 +13,16 @@ puts ""
 
 paytmHASH = Hash.new
 
-paytmHASH["MID"] = 'XXXXXXXXXX';
-paytmHASH["ORDER_ID"] = 'XXXXXXXXXX';
-paytmHASH["CUST_ID"] = 'XXXXXXXXXX';
-paytmHASH["INDUSTRY_TYPE_ID"] = 'XXXXXXXXXX';
-paytmHASH["CHANNEL_ID"] = 'XXXXXXXXXX';
-paytmHASH["TXN_AMOUNT"] = 'XXXXXXXXXX';
-paytmHASH["WEBSITE"] = 'XXXXXXXXXX';
-paytmHASH["EMAIL"] = 'XXXXXXXXXX';
-paytmHASH["MOBILE_NO"] = 'XXXXXXXXXX';
-paytmHASH["CALLBACK_URL"] = 'XXXXXXXXXX';
+paytmHASH["MID"] = 'XXXXXXXXXX';  #Provided by Paytm
+paytmHASH["ORDER_ID"] = 'ORDER0000001'; #unique OrderId for every request
+paytmHASH["CUST_ID"] = 'CUST00001'; # unique customer identifier 
+paytmHASH["INDUSTRY_TYPE_ID"] = 'XXXXXXXXX'; #Provided by Paytm  
+paytmHASH["CHANNEL_ID"] = 'WAP'; #Provided by Paytm
+paytmHASH["TXN_AMOUNT"] = '1.00'; #transaction amount
+paytmHASH["WEBSITE"] = 'XXXXXXXXX'; #Provided by Paytm
+paytmHASH["EMAIL"] = 'abc@gmail.com'; #customer email id
+paytmHASH["MOBILE_NO"] = '9999999999'; #customer 10 digit mobile no.
+paytmHASH["CALLBACK_URL"] = 'https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp';  #Provided by Paytm
 
 #paytmparamsnew = paytmHASH
 
