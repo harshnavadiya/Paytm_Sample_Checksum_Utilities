212c20a6153cbd48365f5386dc8c04ea784602dd
Mohit-Aggarwal1 <mohit.aggarwal@paytm.com>
2017-04-05 12:05:46 +0530

    Dot Net checksum kit

--- Dot Net/Checksumcsharp/GenerateChecksum.aspx.cs
+++ Dot Net/Checksumcsharp/GenerateChecksum.aspx.cs
@@ -9,14 +9,16 @@ public partial class GenerateChecksum : System.Web.UI.Page
         try
         {
             Dictionary<string, string> parameters = new Dictionary<string, string>();
-			parameters.Add("MID", "xxxxxxx");
-			parameters.Add("ORDER_ID", "xxxxxxx");
-			parameters.Add("CUST_ID", "xxxxxxx");
-			parameters.Add("CHANNEL_ID", "xxxxxxx");
-			parameters.Add("INDUSTRY_TYPE_ID", "xxxxxxx");
-			parameters.Add("WEBSITE", "xxxxxxx");
-			parameters.Add("TXN_AMOUNT", "xxxxxxx");
-		
+			parameters.Add("MID", "xxxxxxx"); //Provided by Paytm
+			parameters.Add("ORDER_ID", "ORDER000001"); //unique OrderId for every request
+			parameters.Add("CUST_ID", "CUST00001"); // unique customer identifier 
+			parameters.Add("CHANNEL_ID", "WAP"); //Provided by Paytm
+			parameters.Add("INDUSTRY_TYPE_ID", "xxxxxxx"); //Provided by Paytm
+			parameters.Add("WEBSITE", "xxxxxxx"); //Provided by Paytm
+			parameters.Add("TXN_AMOUNT", "1.00"); // transaction amount
+			paramarray.Add("CALLBACK_URL","https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp"); //Provided by Paytm
+			paramarray.Add("EMAIL","abc@gmail.com"); // customer email id
+			paramarray.Add("MOBILE_NO","9999999999"); // customer 10 digit mobile no.
 			string paytmChecksum = "";
 			 
 			paytmChecksum = CheckSum.generateCheckSum(PaytmConstants.MERCHANT_KEY, parameters);
