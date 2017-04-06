1c67f64382217232dd84a591442e14bd37b0a550
MayankGupta1 <mayank1.gupta@paytm.com>
2017-04-06 11:40:40 +0530

    Dot Net checksum kit

--- Dot Net/Checksumcsharp/GenerateChecksum.aspx.cs
+++ Dot Net/Checksumcsharp/GenerateChecksum.aspx.cs
@@ -9,24 +9,33 @@ public partial class GenerateChecksum : System.Web.UI.Page
         try
         {
             Dictionary<string, string> parameters = new Dictionary<string, string>();
-			parameters.Add("MID", "xxxxxxx"); //Provided by Paytm
-			parameters.Add("ORDER_ID", "ORDER000001"); //unique OrderId for every request
-			parameters.Add("CUST_ID", "CUST00001"); // unique customer identifier 
-			parameters.Add("CHANNEL_ID", "WAP"); //Provided by Paytm
-			parameters.Add("INDUSTRY_TYPE_ID", "xxxxxxx"); //Provided by Paytm
-			parameters.Add("WEBSITE", "xxxxxxx"); //Provided by Paytm
-			parameters.Add("TXN_AMOUNT", "1.00"); // transaction amount
-			paramarray.Add("CALLBACK_URL","https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp"); //Provided by Paytm
-			paramarray.Add("EMAIL","abc@gmail.com"); // customer email id
-			paramarray.Add("MOBILE_NO","9999999999"); // customer 10 digit mobile no.
-			string paytmChecksum = "";
-			 
-			paytmChecksum = CheckSum.generateCheckSum(PaytmConstants.MERCHANT_KEY, parameters);
-			Response.Write(paytmChecksum);
+            parameters.Add("MID", "xxxxxxx"); //Provided by Paytm
+            parameters.Add("ORDER_ID", "ORDER000001"); //unique OrderId for every request
+            parameters.Add("CUST_ID", "CUST00001"); // unique customer identifier 
+            parameters.Add("CHANNEL_ID", "WAP"); //Provided by Paytm
+            parameters.Add("INDUSTRY_TYPE_ID", "xxxxxxx"); //Provided by Paytm
+            parameters.Add("WEBSITE", "xxxxxxx"); //Provided by Paytm
+            parameters.Add("TXN_AMOUNT", "1.00"); // transaction amount
+            parameters.Add("CALLBACK_URL", "https://xxxxxxx.xx/paytmCallback.jsp"); //Provided by Paytm
+            parameters.Add("EMAIL", "abc@gmail.com"); // customer email id
+            parameters.Add("MOBILE_NO", "9999999999"); // customer 10 digit mobile no.
+            string paytmChecksum = "";
 
-		}
-		catch (Exception)
-		{
-		}
+            foreach (string key in parameters.Keys)
+            {
+                if (parameters[key].ToUpper().Contains("REFUND") || parameters[key].ToUpper().Contains("|"))
+                {
+                    parameters[key] = "";
+                }
+            }
+
+            paytmChecksum = CheckSum.generateCheckSum(PaytmConstants.MERCHANT_KEY, parameters);
+            Response.Write(paytmChecksum);
+
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
     }
 }
